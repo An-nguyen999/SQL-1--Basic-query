2edd0ece1c308290100a1b0707285b6e18533adc
An (Selina) Nguyen <163428523+An-nguyen999@users.noreply.github.com>
2024-04-23 22:48:50 +0700

    Add files via upload

---
+++ SQLQuery-Learning-HowKteam.sql
@@ -0,0 +1,34 @@
+﻿USE [Learning SQL-Kteam]
+-- Bắt buộc phải thêm dòng này để biết đang chạy trên Query nào
+
+CREATE TABLE GiaoVien
+(
+MaGV NVARCHAR(10),
+NameGV NVARCHAR(100)
+)	
+go
+-- Tạo bảng
+CREATE TABLE HocSinh
+(
+MaHS NVARCHAR(10),
+NameHS NVARCHAR(100)
+)
+go
+-- Thêm 1 cột khác trong bẳng
+Alter table HocSinh ADD Ngaysinh DATE
+
+
+-- Xóa dữ liệu trong bảng
+-- Truncate table HocSinh
+
+-- xóa bảng
+-- drop table HocSinh
+-- go
+
+
+insert HocSinh	
+values ( N'3',
+		N'Ai đó',
+		'20000909'
+		)
+Delete HocSinh where NameHS = 'Tú Tú'
